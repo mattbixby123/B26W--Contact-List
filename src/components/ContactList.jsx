a10408aa53f4a738d57caf557106e04f8619d075
Jarzembak <lee.jarzembak@gmail.com>
2024-02-15 21:58:12 -0500

    Rendered dummy contacts with ContactRow component

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,9 +1,18 @@
-import React from "react";
 import { useState, useEffect } from "react";
+import ContactRow from './ContactRow.jsx'
+
+const dummyContacts = [
+  { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
+  { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
+  { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
+];
 
 
+function ContactList() {
 
-function ContactList({ contacts }) {
+  const [contacts, setContacts] = useState(dummyContacts)
+
+  console.log("Contacts: ", contacts)
 
 
   return ( 
@@ -20,8 +29,10 @@ function ContactList({ contacts }) {
           <td>Phone</td>
         </tr>
         {
-           // Map over data here - We will 'escape' into Javascript and map over an array of contacts here.
-         }
+          contacts.map((contact) => {
+            return <ContactRow key={contact.id} contact={contact} />;
+          })
+        }
       </tbody>
     </table>
 ); 
